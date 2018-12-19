e50a96528639233adc626f596d6c22f60ca8e01e
wonseok <t01020625129@gmail.com>
2018-12-19 16:47:13 +0900

    WonSeok 브랜치로 병합.

--- FrmTrain.cs
+++ FrmTrain.cs
@@ -17,14 +17,14 @@ namespace RailoNailo
         {
             InitializeComponent();
         }
-        private string getjson()
-        {
-            int pageNo = 10;
-            int pageNum = 1;
-            string depPlaceId = "NAT010000"; //출발지 ID
-            string arrPlaceId = "NAT011668"; //도착지 ID
-            string depPlanTime = "20181201";
-            string serverurl = "http://openapi.tago.go.kr/openapi/service/TrainInfoService/getStrtpntAlocFndTrainInfo?serviceKey=wUgEE5qg4tz1YpeF9csbrTyZ5vsk1AcTEGd%2B7cpaie8Fl8ZCahq3rrl1Z1ZdFdX2xqRRcjzo00AGc%2BgjM3AppA%3D%3D&pageSize=20&pageNo=" + pageNo + "&startPage=" + pageNum + "&depPlaceId="+ depPlaceId + "&arrPlaceId="+arrPlaceId+"&depPlandTime="+depPlanTime+"&_type=json";
-        }
+        //private string getjson()
+        //{
+        //    int pageNo = 10;
+        //    int pageNum = 1;
+        //    string depPlaceId = "NAT010000"; //출발지 ID
+        //    string arrPlaceId = "NAT011668"; //도착지 ID
+        //    string depPlanTime = "20181201";
+        //    string serverurl = "http://openapi.tago.go.kr/openapi/service/TrainInfoService/getStrtpntAlocFndTrainInfo?serviceKey=wUgEE5qg4tz1YpeF9csbrTyZ5vsk1AcTEGd%2B7cpaie8Fl8ZCahq3rrl1Z1ZdFdX2xqRRcjzo00AGc%2BgjM3AppA%3D%3D&pageSize=20&pageNo=" + pageNo + "&startPage=" + pageNum + "&depPlaceId="+ depPlaceId + "&arrPlaceId="+arrPlaceId+"&depPlandTime="+depPlanTime+"&_type=json";
+        //}
     }
 }
