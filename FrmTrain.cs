f91f0b7ceb2204878f34d05b218f6e676eb674b4
wonseok <t01020625129@gmail.com>
2018-12-19 17:00:57 +0900

    DB수정>>원석

--- FrmTrain.cs
+++ FrmTrain.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,7 +25,12 @@ namespace RailoNailo
         //    string depPlaceId = "NAT010000"; //출발지 ID
         //    string arrPlaceId = "NAT011668"; //도착지 ID
         //    string depPlanTime = "20181201";
-        //    string serverurl = "http://openapi.tago.go.kr/openapi/service/TrainInfoService/getStrtpntAlocFndTrainInfo?serviceKey=wUgEE5qg4tz1YpeF9csbrTyZ5vsk1AcTEGd%2B7cpaie8Fl8ZCahq3rrl1Z1ZdFdX2xqRRcjzo00AGc%2BgjM3AppA%3D%3D&pageSize=20&pageNo=" + pageNo + "&startPage=" + pageNum + "&depPlaceId="+ depPlaceId + "&arrPlaceId="+arrPlaceId+"&depPlandTime="+depPlanTime+"&_type=json";
+        //    string serverurl = "http://openapi.tago.go.kr/openapi/service/TrainInfoService/getStrtpntAlocFndTrainInfo?serviceKey=wUgEE5qg4tz1YpeF9csbrTyZ5vsk1AcTEGd%2B7cpaie8Fl8ZCahq3rrl1Z1ZdFdX2xqRRcjzo00AGc%2BgjM3AppA%3D%3D&pageSize=20&pageNo=" + pageNo + "&startPage=" + pageNum + "&depPlaceId=" + depPlaceId + "&arrPlaceId=" + arrPlaceId + "&depPlandTime=" + depPlanTime + "&_type=json";
+
+        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serverurl);
+        //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        //    string statusCode=response.StatusCode
         //}
     }
 }
